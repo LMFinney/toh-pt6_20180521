31bd9920753cdbaec37a760457ce403042ec0f00
Lance Finney <lance.finney@oasisdigital.com>
2018-07-26 10:12:08 -0500

    Test DashboardComponent as though it's a service

--- src/app/dashboard/dashboard.component.spec.ts
+++ src/app/dashboard/dashboard.component.spec.ts
@@ -1,14 +1,11 @@
-import { ComponentFixture, TestBed, async } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+import { TestBed, async } from '@angular/core/testing';
 import { of } from 'rxjs';
 
-import { HeroSearchComponent } from '../hero-search/hero-search.component';
 import { HeroService } from '../hero.service';
 import { DashboardComponent } from './dashboard.component';
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
-  let fixture: ComponentFixture<DashboardComponent>;
 
   beforeEach(async(() => {
     const svcSpy = jasmine.createSpyObj<HeroService>(['getHeroes']);
@@ -19,20 +16,16 @@ describe('DashboardComponent', () => {
     ]));
 
     TestBed.configureTestingModule({
-      declarations: [DashboardComponent, HeroSearchComponent],
-      imports: [RouterTestingModule.withRoutes([])],
       providers: [
+        DashboardComponent,
         { provide: HeroService, useValue: svcSpy }
       ],
-      // schemas: [ CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA ]
     })
       .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(DashboardComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    component = TestBed.get(DashboardComponent);
   });
 
   it('should be created', () => {
@@ -40,10 +33,7 @@ describe('DashboardComponent', () => {
   });
 
   it('should skip the first hero', () => {
+    component.ngOnInit();
     expect(component.heroes).toEqual([{id: 1, name: 'Thatsmy Girl'}, {id: 2, name: 'Soccer Mom'}]);
-  });
-
-  it('should display 1 link', () => {
-    expect(fixture.nativeElement.querySelectorAll('a').length).toBe(2);
   });
 });
